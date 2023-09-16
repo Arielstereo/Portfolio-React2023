2a3653ba148d7ec4ca62b2f75cee116cea1ef083
Ariel Martinez <arielstereo@msn.com>
2023-09-16 13:45:43 -0300

    remove atropos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,18 +2,16 @@ import "./header.css";
 import HeaderBtn from "./HeaderBtn";
 import Dev from "../../assets/logo_transparent.png";
 import HeaderSocials from "./HeaderSocials";
-import Atropos from 'atropos/react';
+
 
 
 const Header = () => {
   return (
     <header id="#">
       <div className="container">
-        <Atropos>
         <div className="img__container">
           <img className="img" src={Dev} alt="Dev-img" />
         </div>
-        </Atropos >
         <HeaderSocials />
         <HeaderBtn />
       </div>
