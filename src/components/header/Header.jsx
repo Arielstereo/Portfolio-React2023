7019c7b5450dfa1a881af482a28d71f3a0bfba03
Ariel Martinez <arielstereo@msn.com>
2024-02-13 18:57:41 -0300

    fix image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,9 +16,9 @@ const Header = () => {
         >
           <img className="img" src={Dev} alt="Dev-img" />
         </motion.div>
-        <motion.div  initial={{ x: 0 }}
-        animate={{ x: 2000 }}
-        transition={{ ease: "easeOut", duration: 50 }} className="loader_pacman"></motion.div>
+        <motion.div  initial={{ x: 0, opacity: 1}}
+        animate={{ x: 1200,opacity:0}}
+        transition={{ duration: 15 }} className="loader_pacman"></motion.div>
         <HeaderSocials />
         <HeaderBtn />
       </div>
